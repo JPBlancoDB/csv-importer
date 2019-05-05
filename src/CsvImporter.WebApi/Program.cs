94da5af5a5ddb940f4e70d0403169b1ee05209b6
JPBlancoDB <jpblancodb@gmail.com>
2019-05-05 20:06:37 +0200

    Add nuget packages, add key vault and appInsights configuration

--- src/CsvImporter.WebApi/Program.cs
+++ src/CsvImporter.WebApi/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace CsvImporter.WebApi
 {
@@ -19,6 +16,17 @@ namespace CsvImporter.WebApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var keyVaultName = config.Build()["KeyVault:Name"];
+                    config.AddAzureKeyVault($"https://{keyVaultName}.vault.azure.net/", CreateKeyVaultClient(), new DefaultKeyVaultSecretManager());
+                })
                 .UseStartup<Startup>();
+        
+        private static KeyVaultClient CreateKeyVaultClient()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            return new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+        }
     }
 }
--- tests/CsvImporter.WebApi.Tests/ImporterControllerTests.cs
+++ tests/CsvImporter.WebApi.Tests/ImporterControllerTests.cs
@@ -1,9 +1,8 @@
-using System;
 using Xunit;
 
 namespace CsvImporter.WebApi.Tests
 {
-    public class UnitTest1
+    public class ImporterControllerTests
     {
         [Fact]
         public void Test1()
