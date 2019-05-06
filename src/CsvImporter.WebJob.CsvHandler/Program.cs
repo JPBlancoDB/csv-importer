2de584a1870ae76559203ea96141ceb42851c964
JPBlancoDB <jpblancodb@gmail.com>
2019-05-06 22:05:19 +0200

    Add CsvHandler webjob template

---
+++ src/CsvImporter.WebJob.CsvHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvImporter.WebJob.CsvHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
